9f8efc2a3655e2c901337cf2e16f2dd851877587
templar-ajay <templar.ajay@gmail.com>
2023-12-01 11:53:41 +0530

    gtag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -56,28 +56,25 @@ export default async function RootLayout({
   const client = createClient();
   const settings = await client.getSingle("settings");
   const {
-    data: { primary_color, secondary_color, gtm_id },
+    data: { primary_color, secondary_color, gtm_id: GTM_ID },
   } = settings;
   return (
     <html lang="en">
-      <Head>
-        {gtm_id?.length && (
-          <Script id="google-analytics">
-            {`
-          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','${gtm_id}');
+      <Script src={`https://www.googletagmanager.com/gtag/js?id=${GTM_ID}`} />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', '${GTM_ID}');
         `}
-          </Script>
-        )}
-      </Head>
+      </Script>
       <body className={clsx(body.variable, display.variable)}>
-        {gtm_id?.length && (
+        {GTM_ID?.length && (
           <noscript>
             <iframe
-              src={`https://www.googletagmanager.com/ns.html?id=${gtm_id}`}
+              src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
               height="0"
               width="0"
               style={{ display: "none", visibility: "hidden" }}
